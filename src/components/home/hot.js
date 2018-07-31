8fc7bf98d3db021c580170eb25fc5250402dd8c3
王振福 <wangzhenfu@zto.ldap>
2018-07-31 22:07:53 +0800

    update HotTop

--- src/components/home/hot.js
+++ src/components/home/hot.js
@@ -7,21 +7,26 @@ class Hot extends React.Component<{}> {
   render() {
     return (
       <div className="hot">
-        <p className="hot-header-list">
-          <button className="active">推荐</button>
-          <button>附近</button>
-          <button>榜单</button>
-          <button>明星</button>
-          <button>搞笑</button>
-          <button>情感</button>
-          <button>新时代</button>
-          <button>社会</button>
-          <button>上海</button>
-          <button>电影</button>
-          <button>游戏</button>
-          <button>汽车</button>
-          <button>星座</button>
-        </p>
+        <div className="hot-header">
+          <p className="hot-header-list">
+            <button className="active">推荐</button>
+            <button>附近</button>
+            <button>榜单</button>
+            <button>明星</button>
+            <button>搞笑</button>
+            <button>情感</button>
+            <button>新时代</button>
+            <button>社会</button>
+            <button>上海</button>
+            <button>电影</button>
+            <button>游戏</button>
+            <button>汽车</button>
+            <button>星座</button>
+          </p>
+          <span>
+            <i className="iconfont icon-add rem10"></i>
+          </span>
+        </div>
       </div>
     )
   }
